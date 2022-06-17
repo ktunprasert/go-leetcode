1fe3c558888dc3255228fd64d9c691882c703c06
ktunprasert <ktunprasert@outlook.com>
2022-06-17 10:25:53 +0100

    numofonebits: naive

---
+++ numofonebits/naive.go
@@ -0,0 +1,15 @@
+package numofonebits
+
+func hammingWeight(num uint32) int {
+	if num == 0 {
+		return 0
+	}
+	count := 0
+	for num > 0 {
+		if num&1 == 1 {
+			count++
+		}
+		num = num >> 1
+	}
+	return count
+}
---
+++ numofonebits/naive_test.go
@@ -0,0 +1,25 @@
+package numofonebits
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"e1", args{0b1011}, 3},
+		{"e2", args{0b10000000}, 1},
+		{"e2", args{0b11111111111111111111111111111101}, 31},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
